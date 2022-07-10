7a2bbd8f55f6d39c7b6514e43a9ebdc2c28f6677
Steve Lee <steve.patriot@gmail.com>
2022-07-10 07:36:21 -0400

    provider consumer with child parameter
     what's going on here? The child widget passed to Consumer is built on it own, outside of the builder callback. It is then passed into the builder as it's third argument. While this API may look a little wonky, it's pretty impressive. It allows the child widget to go on livin', without being rebuilt, while all the widgets defined in the builder callback do get rebuilt. Wow.
    
    Again, the API might seem confusing, because it looks like the consumer now has two properties that build the widget tree below this Consumer. But, once you get used to looking at this, it's not a big deal. And totally worth the benefits.

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:provider/provider.dart';
   Controlling build() cost
   Consumer widget provides more specific rebuilds that improves performance.
   Bob the builder
-  child 부분은 다시 render 하지 않는다.
 
 *
 
@@ -29,28 +28,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     print("MyApp building");
-    return ChangeNotifierProvider(
-      create: (context) => ColorChangerNotifier(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+    return
+      ChangeNotifierProvider(
+        create: (context) => ColourNotifier(),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MyHomePage(title: 'Flutter Demo Home Page'),
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
+      );
+  }
+}
+
+class ColourNotifier with ChangeNotifier {
+  bool _isRed = true;
+  bool get isRed => _isRed;
+
+  void switchColour() {
+    _isRed = !_isRed;
+    notifyListeners();
   }
 }
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
 
   @override
   Widget build(BuildContext context) {
-    print("MyHomePage");
-    //var colorChangeNotifier = Provider.of<ColorChangerNotifier>(context); // 객체 만들었고.. 그객체를 위에서 사용한다고 했고
+    //var counterChangeNotifier = Provider.of<CounterChangeNotifier>(context); // 객체 만들었고.. 그객체를 위에서 사용한다고 했고
     // 여기서 변수로 받았다. 그렇지만 여기가 있어서 build 할 때마다 계속 하위에 있는것들이 전부 다 다시 빌드가 되는거지.. 그래서 // TODO
     return Scaffold(
       appBar: AppBar(
@@ -59,18 +67,33 @@ class MyHomePage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
+          children: <Widget>[
+            const Text(
               'You have pushed the button this many times:',
             ),
-            MyContainer(),
+            Consumer<ColourNotifier>(
+              builder: (context, value, child) {
+                return RedBox(isRed: value.isRed, child: child,); // 여기서 child 를 빼니깐 아예 child 를 패스해버리고 안찍어버리네..
+              },
+              child:  const CounterNumber(number : 0),
+              /*
+               what's going on here?
+               The child widget passed to Consumer is built on it own, outside of the builder callback.
+               It is then passed into the builder as it's third argument.
+               While this API may look a little wonky, it's pretty impressive.
+               It allows the child widget to go on living', without being rebuilt,
+               while all the widgets defined in the builder callback do get rebuilt. Wow.
+
+                Again, the API might seem confusing, because it looks like the consumer now has two properties
+                that build the widget tree below this Consumer.
+                But, once you get used to looking at this, it's not a big deal. And totally worth the benefits.
+               */
+                ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () =>
-            Provider.of<ColorChangerNotifier>(context, listen: false)
-                .changeColor(),
+        onPressed: () => Provider.of<ColourNotifier>(context, listen: false).switchColour(),
         // 괭장히 중요한 부분이다. 내가 만든 클래스의 함수를 실행시킬때 값이 변경되는건 궁금하지 않고 그냥 함수를 받아오고 싶을 때
         // listen : false 로 세팅해준다.
         tooltip: 'Increment',
@@ -80,67 +103,43 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class MyContainer extends StatelessWidget {
-  const MyContainer({
-    Key? key,
-  }) : super(key: key);
+class CounterNumber extends StatelessWidget {
+  final int? number;
+  const CounterNumber({Key? key, required this.number}) : super(key : key);
 
   @override
   Widget build(BuildContext context) {
-    print("MyContainer");
-    //var _color = Provider.of<ColorChangerNotifier>(context).color;
-    return Consumer<ColorChangerNotifier>(
-      builder: (context, value, child) {
-        print("container"); // 여기 보이지? container 는 실행된다. 그러나 밑에 봐라..
-        return Container(
-            color: value._color, padding: const EdgeInsets.all(10),
-          child: const Text("AA"),
-        );
-      },
-      child: const ShowText(), // 이부분 ShowText() 는 실행이 안된다.
-    );
-  }
-}
-
-class ShowText extends StatelessWidget {
-  const ShowText({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    print("ShowText");
+    print('CounterNumber building');
     return Text(
-      "ShowText",
-      //Provider.of<CounterChangeNotifier>(context).counter.toString(),
+      number.toString(),
       style: Theme.of(context).textTheme.headline4,
     );
   }
 }
 
-class CounterChangeNotifier with ChangeNotifier {
-  // setState 대신에 사용할 수 있다.
-  int _counter = 0;
+class RedBox extends StatelessWidget {
+  final Widget? child;
+  final bool? isRed;
+  const RedBox ({Key? key, this.child, this.isRed}) : super(key: key);
 
-  int get counter => _counter;
-
-  increment() {
-    _counter++;
-    notifyListeners();
+  @override
+  Widget build(BuildContext context) {
+    print('RedBox building');
+    return Container( // 조건문
+      color: isRed ?? true ? Colors.redAccent : Colors.blue,
+              // ?? null 이면 true 를 헣고 true 이면
+      width: 50,
+      height: 50,
+      child: Center(child: child,),
+    ); // 잘봐라. return 문에는 항상 세미콜론을 넣어야 한다.
   }
 }
 
-class ColorChangerNotifier with ChangeNotifier {
-  Color _color = Colors.red;
-
-  Color get color => _color;
-
-  changeColor() {
-    if (_color == Colors.red) {
-      _color = Colors.green;
-    } else {
-      _color = Colors.red;
-    }
+class CounterChangeNotifier with ChangeNotifier { // setState 대신에 사용할 수 있다.
+  int _counter = 0;
+  int get counter => _counter;
+  increment() {
+    _counter++;
     notifyListeners();
   }
 }
